42b1b8bfd22a663315523e216d5392420cf5523f
Bartosz <69923253+BartBoxi@users.noreply.github.com>
2023-01-08 23:45:41 +0100

    Create day_8_1.py

---
+++ day_8_1.py
@@ -0,0 +1,18 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+# def greet():
+#   print("Hello User")
+#   print("This is my code")
+#   print("And this is my function ")
+
+# greet()
+
+#Function that allows for input 
+
+def greet_with_name(name):
+  print(f"Hello User {name}")
+  print(f"This is my code{name}")
+  print(f"And this is my function{name}")
